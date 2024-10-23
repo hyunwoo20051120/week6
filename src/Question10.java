f32f8b2fc20b6b6c7b08dcf85fb7f9b31f1bb839
hyunwoo <hyunwoo20051120@gmail.com>
2024-10-23 16:36:47 +0900

    k

--- src/Question10.java
+++ src/Question10.java
@@ -2,7 +2,8 @@ public class Question10 {
     public static void main(String[] args) {
         int[] numbers = {20,30,50,70,90};
         int sum = 0;
-        int average;
+        double average;
+
 
         for (int i = 0; i < numbers.length; i++) {
             sum += numbers[i];
